b320a3b24e487b1b88da7ff0805684cefc65a69d
cmoylan <chris@chrismoylan.com>
2010-11-27 02:02:00 -0600

    removed now() from datetime field in page because sqlite cant handle it

--- chrismoylan/model/__init__.py
+++ chrismoylan/model/__init__.py
@@ -4,6 +4,8 @@ from chrismoylan.model.blog import Blog, BlogTag
 from chrismoylan.model.comment import Comment
 from chrismoylan.model.tag import Tag
 from chrismoylan.model.user import User
+from chrismoylan.model.page import Page
+from chrismoylan.model.portfolio import Portfolio
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
--- chrismoylan/model/page.py
+++ chrismoylan/model/page.py
@@ -3,12 +3,14 @@ from sqlalchemy import Table, Column, ForeignKey
 from sqlalchemy.types import Integer, Unicode, UnicodeText, DateTime, Boolean
 from sqlalchemy.orm import relation
 from chrismoylan.model.meta import Base
+from datetime import datetime
 
 class Page(Base):
     __tablename__ = "page"
     
     id = Column(Integer, primary_key=True)
-    posted = Column(DateTime, default=u'now()')
+    #posted = Column(DateTime, default=u'now()')
+    posted = Column(DateTime, default=datetime.now())
     title = Column(Unicode(255), default=u'Untitled')
     content = Column(Unicode, nullable=False)
     
