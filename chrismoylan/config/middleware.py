2d0ba206f3db1e49b5f1e643d3908d300890dd36
cmoylan <chris@chrismoylan.com>
2010-12-03 01:20:31 -0600

    removed repoze.who =(

--- chrismoylan/config/middleware.py
+++ chrismoylan/config/middleware.py
@@ -47,13 +47,13 @@ def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
     # CUSTOM MIDDLEWARE HERE (filtered by error handling middlewares)
 
     # Repoze.who
-    app = make_who_with_config(
-        app,
-        global_conf,
-        app_conf['who.config_file'],
-        app_conf['who.log_file'],
-        app_conf['who.log_level']
-    )
+    #app = make_who_with_config(
+    #    app,
+    #    global_conf,
+    #    app_conf['who.config_file'],
+    #    app_conf['who.log_file'],
+    #    app_conf['who.log_level']
+    #)
 
     if asbool(full_stack):
         # Handle Python exceptions
